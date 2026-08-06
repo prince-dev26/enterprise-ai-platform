5744baa1b84be1f8f37b76376c163fcc272189b7
prince-dev26 <princekumar21791@gmail.com>
2026-08-06 11:33:57 -0400

    First API

--- enterprise-ai-platform/backend/app/main.py
+++ enterprise-ai-platform/backend/app/main.py
@@ -1,5 +1,25 @@
+# from fastapi import FastAPI
+
+# from app.core.config import settings
+
+# app = FastAPI(
+#     title=settings.APP_NAME,
+#     version=settings.APP_VERSION,
+# )
+
+
+# @app.get("/")
+# def root():
+#     return {
+#         "message": settings.APP_NAME,
+#         "version": settings.APP_VERSION,
+#         "debug": settings.DEBUG,
+#     }
+
+
 from fastapi import FastAPI
 
+from app.api.router import api_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -7,11 +27,4 @@ app = FastAPI(
     version=settings.APP_VERSION,
 )
 
-
-@app.get("/")
-def root():
-    return {
-        "message": settings.APP_NAME,
-        "version": settings.APP_VERSION,
-        "debug": settings.DEBUG,
-    }
+app.include_router(api_router)
