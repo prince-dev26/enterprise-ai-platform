8e2fcbcf57fad27ca94a0eb94743c62322ec58d6
prince-dev26 <princekumar21791@gmail.com>
2026-08-07 10:08:43 -0400

    Write Base in Databse

--- enterprise-ai-platform/backend/app/core/database.py
+++ enterprise-ai-platform/backend/app/core/database.py
@@ -2,3 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.core.config import settings
+
+
+engine = create_engine(
+    settings.DATABASE_URL,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+class Base(DeclarativeBase):
+    pass
