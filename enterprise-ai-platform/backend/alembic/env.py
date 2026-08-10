9ab236920df6efbf7939f9ccabaff793d55a2904
prince-dev26 <princekumar21791@gmail.com>
2026-08-10 10:21:13 -0400

    Some changes

--- enterprise-ai-platform/backend/alembic/env.py
+++ enterprise-ai-platform/backend/alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from app.core.config import settings
 from app.core.database import Base
 from app.models.organization import Organization
+from app.models.user import User
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
