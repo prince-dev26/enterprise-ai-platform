1b7d5130562ed3090e568c0050ac8d86d7d079be
prince-dev26 <princekumar21791@gmail.com>
2026-08-11 10:26:50 -0400

    Update User in services

---
+++ enterprise-ai-platform/backend/app/services/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.repositories.user import UserRepository
+from app.schemas.user import UserCreate
+
+
+class UserService:
+
+    def __init__(self, db: Session):
+        self.repository = UserRepository(db)
+
+    def create(self, data: UserCreate) -> User:
+        existing_user = self.repository.get_by_email(
+            email=data.email,
+            organization_id=data.organization_id,
+        )
+
+        if existing_user:
+            raise ValueError("User email already exists")
+
+        user = User(
+            organization_id=data.organization_id,
+            email=data.email,
+            password_hash="TEMPORARY",
+            first_name=data.first_name,
+            last_name=data.last_name,
+        )
+
+        return self.repository.create(user)
